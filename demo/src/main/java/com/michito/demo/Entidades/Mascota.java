ba7e97ff1aabc7a96ad6542f952f4505d958ada7
CARLOSECG _ <caicedog.ce@javeriana.edu.co>
2024-08-16 23:07:02 +0000

    Cliente

--- demo/src/main/java/com/michito/demo/Entidades/Mascota.java
+++ demo/src/main/java/com/michito/demo/Entidades/Mascota.java
@@ -1,17 +1,23 @@
 package com.michito.demo.Entidades;
 
 public class Mascota {
+    private int id;
     private String nombre;
     private int edad;
     private int peso;
     private String enfermedad;
     private String foto;
-    public Mascota(String nombre,int edad,int peso,String enfermedad,String foto){
+    
+    public Mascota(int id,String nombre,int edad,int peso,String enfermedad,String foto){
+        this.id=id;
         this.nombre=nombre;
         this.edad=edad;
         this.peso=peso;
         this.enfermedad=enfermedad;
         this.foto=foto;
+    }
+    public int getId(){
+        return id;
     }
     public String getNombre() {
         return nombre;
@@ -27,6 +33,9 @@ public class Mascota {
     }
     public String getFoto() {
         return foto;
+    }
+    public void setId(int id){
+        this.id=id;
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
