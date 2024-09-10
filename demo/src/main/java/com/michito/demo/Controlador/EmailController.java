544c3ed2923e1698087a8e9ac15a004c0e6a9316
Sergio Asencio <sergioasenciorodriguez@gmail.com>
2024-09-10 18:56:11 -0500

    Email Sender Funcional

---
+++ demo/src/main/java/com/michito/demo/Controlador/EmailController.java
@@ -0,0 +1,19 @@
+package com.michito.demo.Controlador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.michito.demo.Servicio.ServicioEmail;
+@Controller
+public class EmailController {
+
+    @Autowired
+    private ServicioEmail emailService;
+
+    @GetMapping("/send-email")
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
+        emailService.sendEmail(to, subject, body);
+        return "Email enviado con éxito a " + to;
+    }
+}
