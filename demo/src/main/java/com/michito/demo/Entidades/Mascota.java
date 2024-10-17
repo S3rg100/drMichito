c9c50b5ede47c8e22db7e67b94c42fc040a5bcd0
00CARLOSECG00 <caicedog.ce@javeriana.edu.co>
2024-10-17 09:33:00 -0500

    Eliminar cliente

--- demo/src/main/java/com/michito/demo/Entidades/Mascota.java
+++ demo/src/main/java/com/michito/demo/Entidades/Mascota.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PreRemove;
 import jakarta.persistence.Transient;
 
 @Entity
@@ -24,35 +25,47 @@ public class Mascota {
     private int peso;
     private int edad;
     private String foto;
+
     @Transient // No se almacena en la base de datos
     private String cedulaCliente;
+
     @JsonIgnore
     @ManyToOne(fetch = FetchType.EAGER)
     private Cliente cliente;
+
     @JsonIgnore
     @OneToMany(mappedBy = "mascota")
     private List<Tratamieneto> tratamientos = new ArrayList<>();
     
+    // Método PreRemove para romper la relación con los tratamientos antes de eliminar la mascota
+    @PreRemove
+    public void preRemove() {
+        for (Tratamieneto tratamiento : this.tratamientos) {
+            tratamiento.setMascota(null);  // Rompe la relación con los tratamientos
+        }
+    }
+    
     public List<Tratamieneto> getTratamientos() {
         return tratamientos;
     }
+    
     public void setTratamientos(List<Tratamieneto> tratamientos) {
         this.tratamientos = tratamientos;
     }
-    
-
-
    
     public Mascota(){}
-    public Mascota(String nombre,int edad,int peso ,String foto){
-        this.nombre=nombre;
-        this.edad=edad;
-        this.peso=peso;
-        this.foto=foto;
+    
+    public Mascota(String nombre, int edad, int peso, String foto) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.foto = foto;
     }
+
     public Cliente getCliente() {
         return cliente;
     }
+
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
@@ -65,18 +78,23 @@ public class Mascota {
     public String getNombre() {
         return nombre;
     }
+
     public int getEdad() {
         return edad;
     }
+
     public int getPeso() {
         return peso;
     }
+
     public String getFoto() {
         return foto;
     }
-    public void setId(Long id){
-        this.id=id;
+
+    public void setId(Long id) {
+        this.id = id;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -84,9 +102,11 @@ public class Mascota {
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
     public void setPeso(int peso) {
         this.peso = peso;
     }
+
     public void setFoto(String foto) {
         this.foto = foto;
     }
@@ -98,5 +118,4 @@ public class Mascota {
     public void setCedulaCliente(String cedulaCliente) {
         this.cedulaCliente = cedulaCliente;
     }
-   
 }
