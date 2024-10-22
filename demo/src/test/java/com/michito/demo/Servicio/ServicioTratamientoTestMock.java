13ec42de6ca4315be3d64c780e23e730a5f7fb22
00CARLOSECG00 <caicedog.ce@javeriana.edu.co>
2024-10-22 16:46:53 -0500

    Servicio

--- demo/src/test/java/com/michito/demo/Servicio/ServicioTratamientoTestMock.java
+++ demo/src/test/java/com/michito/demo/Servicio/ServicioTratamientoTestMock.java
@@ -6,6 +6,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
@@ -23,6 +27,7 @@ import com.michito.demo.Entidades.Mascota;
 import com.michito.demo.Entidades.Tratamieneto;
 import com.michito.demo.Entidades.Veterinario;
 import com.michito.demo.Repositorio.TratamientoRepositorio;
+import com.michito.demo.Repositorio.VeterinarioRepositorio;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -36,6 +41,8 @@ public class ServicioTratamientoTestMock {
 
     @Mock
     TratamientoRepositorio tratamientoRepositorio;
+    @Mock
+    VeterinarioRepositorio veterinarioRepositorio;
     //13
     @Test
     public void ServicioTratamiento_addTratamiento_Tratamiento(){
@@ -49,39 +56,44 @@ public class ServicioTratamientoTestMock {
     }
     //14
     @Test
-    public void ServicioTratamiento_obtenerTratamientosPorVeterinario_TratamientoList(){
-        //arrange
-        Tratamieneto tratamieneto1= new Tratamieneto(LocalDate.of(2024,5,15),"Nuevo");
-        Tratamieneto tratamieneto2= new Tratamieneto(LocalDate.of(2024,8,15),"nuevo 2");
-        Veterinario veterinario= new Veterinario("1111","pablo","corr@gm.com",456L,"cirujano",true);
+    public void ServicioTratamiento_obtenerTratamientosPorVeterinario_TratamientoList() {
+        // arrange
+        Tratamieneto tratamiento1 = new Tratamieneto(LocalDate.of(2024, 5, 15), "Nuevo");
+        Tratamieneto tratamiento2 = new Tratamieneto(LocalDate.of(2024, 8, 15), "Nuevo 2");
+        Veterinario veterinario = new Veterinario("1111", "Pablo", "corr@gm.com", 456L, "cirujano", true);
+        
+        when(tratamientoRepositorio.save(tratamiento1)).thenReturn(tratamiento1);
+        when(tratamientoRepositorio.save(tratamiento2)).thenReturn(tratamiento2);
+        when(veterinarioRepositorio.save(veterinario)).thenReturn(veterinario);
+        
         servicioVeterinario.addVeterinario(veterinario);
-        tratamieneto1.setVeterinario(veterinario);
-        tratamieneto2.setVeterinario(veterinario);
-        servicioTratamiento.addTratamiento(tratamieneto1);
-        servicioTratamiento.addTratamiento(tratamieneto2);
-        //act
-        List<Tratamieneto>tratamientos = servicioTratamiento.obtenerTratamientosPorVeterinario(veterinario.getId());
-        //assert
+        tratamiento1.setVeterinario(veterinario);
+        tratamiento2.setVeterinario(veterinario);
+        servicioTratamiento.addTratamiento(tratamiento1);
+        servicioTratamiento.addTratamiento(tratamiento2);
+        when(tratamientoRepositorio.findByVeterinarioId(veterinario.getId())).thenReturn(Arrays.asList(tratamiento1, tratamiento2));
+        
+        List<Tratamieneto> tratamientos = servicioTratamiento.obtenerTratamientosPorVeterinario(veterinario.getId());
+        
         Assertions.assertThat(tratamientos).isNotNull();
         Assertions.assertThat(tratamientos.size()).isGreaterThan(0);
         Assertions.assertThat(tratamientos.size()).isEqualTo(2);
     }
-    //15
+    
+    //15 Prueba para el método updateById
     @Test
-    public void ServicioTratamiento_obtenerTratamientosPorMascota_TratamientoList(){
-        Mascota mascota = new Mascota("mike",4,5,"URL",true);
-        Tratamieneto tratamieneto1= new Tratamieneto(LocalDate.of(2024,5,15),"Nuevo");
-        Tratamieneto tratamieneto2= new Tratamieneto(LocalDate.of(2024,8,15),"nuevo 2");
-        servicioMascota.addMascota(mascota);
-        tratamieneto1.setMascota(mascota);
-        tratamieneto2.setMascota(mascota);
-        servicioTratamiento.addTratamiento(tratamieneto1);
-        servicioTratamiento.addTratamiento(tratamieneto2);
-        List<Tratamieneto>tratamientos = servicioTratamiento.obtenerTratamientosPorMascota(mascota.getId());
-        Assertions.assertThat(tratamientos).isNotNull();
-        Assertions.assertThat(tratamientos.size()).isGreaterThan(0);
-        Assertions.assertThat(tratamientos.size()).isEqualTo(2);
+    void testUpdateById() {
+        Tratamieneto tratamientoExistente = new Tratamieneto(LocalDate.of(2024, 5, 12), "Nuevo tratamiento");
+
+        when(tratamientoRepositorio.save(tratamientoExistente)).thenReturn(tratamientoExistente);
+        servicioTratamiento.addTratamiento(tratamientoExistente);
+        tratamientoExistente.setDescripcion("CAMBIA");
+        when(tratamientoRepositorio.save(tratamientoExistente)).thenReturn(tratamientoExistente);
+        Tratamieneto modificado = servicioTratamiento.updateTratamiento(tratamientoExistente.getId(), tratamientoExistente);
+        Assertions.assertThat(modificado).isNotNull();  
+        Assertions.assertThat(modificado.getDescripcion()).isEqualTo("CAMBIA");
     }
+
 
     
 }
